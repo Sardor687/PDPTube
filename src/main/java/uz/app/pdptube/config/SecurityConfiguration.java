5f4960f4e7cfccd9daebded771034e3cb6689f6c
Sardorbek <sardorolimjonov0608@gmail.com>
2025-01-24 17:01:38 +0500

    cors config is moved to securtiyConfig, for frontend

--- src/main/java/uz/app/pdptube/config/SecurityConfiguration.java
+++ src/main/java/uz/app/pdptube/config/SecurityConfiguration.java
@@ -8,7 +8,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import uz.app.pdptube.repository.UserRepository;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,7 +24,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .cors(cors -> cors.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.requestMatchers(
                                 "/auth/**",
@@ -41,5 +46,20 @@ public class SecurityConfiguration {
     @Bean
     public UserDetailsService userDetailsService() {
         return email -> userRepository.findByEmail(email).orElseThrow();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:8080",
+                "https://strong-lynelle-wjmlvbcy-6cdaec0b.koyeb.app",
+                "http://localhost:5173"));
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
