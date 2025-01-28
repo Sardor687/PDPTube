c5fb77e24fbf896b1f08d742951ef5f2991500ba
Sardorbek <sardorolimjonov0608@gmail.com>
2025-01-28 16:51:13 +0500

    age removed form getvodeos 1000

--- src/main/java/uz/app/pdptube/service/VideoService.java
+++ src/main/java/uz/app/pdptube/service/VideoService.java
@@ -77,18 +77,18 @@ public class VideoService {
     }
     public ResponseMessage getVideos() {
         List<Video> videos = videoRepository.findAll();
-        boolean isRegistered = true;
-        if (Helper.getCurrentPrincipal() == null) {
-            isRegistered = false;
-        }
-        Integer age;
-        if (isRegistered) {
-            age = Helper.getCurrentPrincipal().getAge();
-        } else {
-            age = 1000;
-        }
+//        boolean isRegistered = true;
+//        if (Helper.getCurrentPrincipal() == null) {
+//            isRegistered = false;
+//        }
+//        Integer age;
+//        if (isRegistered) {
+//            age = Helper.getCurrentPrincipal().getAge();
+//        } else {
+//            age = 1000;
+//        }
         List<Video> filteredVideos = videos.stream()
-                .filter(video -> video.getAgeRestriction() <= age)
+//                .filter(video -> video.getAgeRestriction() <= age)
                 .filter(video -> {
                     return fileUploaded(video.getVideoLink());
                 })
